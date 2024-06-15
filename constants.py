ed6abde31b68a79c2216983ea1a68ed7b623dd9d
Andrew Mao <andrewyxmao@gmail.com>
2024-06-14 17:02:35 -0700

    better model

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
-GPT4ALL_MODEL_NAME='llama-2-7b-chat.Q4_0.gguf' 
+# GPT4ALL_MODEL_NAME='llama-2-7b-chat.Q4_0.gguf' 
+GPT4ALL_MODEL_NAME='mistral-7b-openorca.gguf2.Q4_0.gguf' 
 
 GPT4ALL_MODEL_FOLDER_PATH='C:/Users/andre/AppData/Local/nomic.ai/GPT4All/'
 GPT4ALL_BACKEND='llama'
